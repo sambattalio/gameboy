9dc7731ea3c1b0ee09e065e89f570e669058a1c2
Noah Yoshida <nyoshida@nd.edu>
2020-01-22 15:52:51 -0500

    Added makefile debug option, added test runner for carry logic

--- src/helpers.h
+++ src/helpers.h
@@ -2,13 +2,14 @@
 #define HELPERS_H
 
 #include <stdio.h>
+#include <stdint.h>
 
-#define DEBUG 1
+#ifndef DEBUG
+#define DEBUG 0
+#endif 
+
 #define debug_print(fmt, ...) \
             do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
-
-#define is_half_carry_add(a, b) ((a & 0xf) + (b & 0xf) > 0xf)
-#define is_half_carry_sub(a, b) (((a & 0xf) - (b & 0xf)) < 0)
 
 int is_half_carry_add(uint8_t a, uint8_t b);
 int is_half_carry_sub(uint8_t a, uint8_t b);
